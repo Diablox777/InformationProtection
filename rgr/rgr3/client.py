0beba48593c07be433dd5765152c619ce03c2b55
Diablox777 <tetyakov2@gmail.com>
2024-12-11 22:30:57 +0700

    Добавил визуальное оформление в консоли

--- rgr/rgr3/client.py
+++ rgr/rgr3/client.py
@@ -1,5 +1,4 @@
 import random
-
 from lib import *
 import socket
 
@@ -10,71 +9,70 @@ class Client:
         self.name = name
         self._buffer_size = 1024
         
-    def register(self, host: str = "localhost", port: int = 3011):
-        print(f"[INFO] Попытка соединения с сервером для регистрации пользователя с ником '{self.name}'...")
+    def register(self, host: str = "localhost", port: int = 3012):
+        print(f"\033[94m[INFO] Попытка соединения с сервером для регистрации пользователя с ником '{self.name}'...\033[0m")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
-        print(f"[INFO] Соединение с сервером установлено")
+        print(f"\033[92m[INFO] Соединение с сервером установлено\033[0m")
         n = int(sock.recv(self._buffer_size).decode("utf8"))
-        print(f"[INFO] С сервера получено n")
+        print(f"\033[94m[INFO] С сервера получено n\033[0m")
    
-        sock.send((bytes("{register}", encoding = "utf8")))
-        sock.send((bytes(self.name, encoding = "utf8")))
+        sock.send((bytes("{register}", encoding="utf8")))
+        sock.send((bytes(self.name, encoding="utf8")))
         
         self.s = gen_mutually_prime_big(n)
         v = exponentiation_modulo(self.s, 2, n)
 
-        sock.send((bytes(str(v), encoding = "utf8")))
+        sock.send((bytes(str(v), encoding="utf8")))
 
         status = sock.recv(self._buffer_size).decode("utf8")
         if status == "success":
-            print(f"Вы были успешно зарегистрированы")
+            print(f"\033[92mВы были успешно зарегистрированы\033[0m")
         elif status == "already registered":
-            print(f"Пользователь с никнеймом '{self.name}' уже зарегистрирован")
+            print(f"\033[91mПользователь с никнеймом '{self.name}' уже зарегистрирован\033[0m")
         
-        print(f"[INFO] Соединение с сервером разорвано\n")
+        print(f"\033[94m[INFO] Соединение с сервером разорвано\033[0m\n")
         sock.close()
 
-    def auth(self, host: str = "localhost", port: int = 3011):
-        print(f"[INFO] Попытка соединения с сервером для авторизации пользователя с ником '{self.name}'...")
+    def auth(self, host: str = "localhost", port: int = 3012):
+        print(f"\033[94m[INFO] Попытка соединения с сервером для авторизации пользователя с ником '{self.name}'...\033[0m")
         sock = socket.socket()
         sock.connect((host, port))
-        print(f"[INFO] Соединение с сервером установлено")
+        print(f"\033[92m[INFO] Соединение с сервером установлено\033[0m")
         
-        sock.send((bytes("{auth}", encoding = "utf8")))
+        sock.send((bytes("{auth}", encoding="utf8")))
         
         n = int(sock.recv(self._buffer_size).decode("utf8"))
-        print(f"[INFO] С сервера получено n")
+        print(f"\033[94m[INFO] С сервера получено n\033[0m")
         
-        sock.send((bytes(self.name, encoding = "utf8")))
+        sock.send((bytes(self.name, encoding="utf8")))
         
         while True:
             r = random.randrange(1, n - 1)
             x = exponentiation_modulo(r, 2, n)
-            sock.send((bytes(str(x), encoding = "utf8")))
+            sock.send((bytes(str(x), encoding="utf8")))
             
             e = int(sock.recv(self._buffer_size).decode("utf8"))
 
             y = r * self.s ** e % n
 
-            sock.send((bytes(str(y), encoding = "utf8")))
+            sock.send((bytes(str(y), encoding="utf8")))
 
             status = sock.recv(self._buffer_size).decode("utf8")
             if status == "success":
-                print("Авторизация пройдена успешно")
+                print("\033[92mАвторизация пройдена успешно\033[0m")
                 break
             elif status == "fail":
-                print("В авторизации отказано. Аутентификация не пройдена")
+                print("\033[91mВ авторизации отказано. Аутентификация не пройдена\033[0m")
                 break
             elif status == "check":
                 pass
 
-        print(f"[INFO] Соединение с сервером разорвано\n")
+        print(f"\033[94m[INFO] Соединение с сервером разорвано\033[0m\n")
         sock.close()
 
 
 if __name__ == '__main__':
-
     alice = Client('Alice')
     alice.register()
     alice.auth()
--- rgr/rgr3/server.py
+++ rgr/rgr3/server.py
@@ -5,7 +5,7 @@ import signal
 
 
 class Server:
-    def __init__(self, host: str = "localhost", port: int = 3011):
+    def __init__(self, host: str = "localhost", port: int = 3012):
         self._host = host
         self._port = port
         self._buffer_size = 1024
@@ -21,27 +21,26 @@ class Server:
         try:
             with open("registered_users.json", "r") as f:
                 self._registered_users = json.load(f)
-            print("[INFO] Загружена база с пользователями")
+            print("\033[92m[INFO] Загружена база с пользователями\033[0m")
         except FileNotFoundError:
             with open("registered_users.json", "w") as f:
                 self._registered_users = dict()
                 json.dump(self._registered_users, f)
-            print(
-                "[ERROR] Не удалось загрузить базу с пользователями. Была создана новая")
+            print("\033[91m[ERROR] Не удалось загрузить базу с пользователями. Была создана новая\033[0m")
 
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((self._host, self._port))
         sock.listen()
-        print("[INFO] Сервер запущен")
+        print("\033[92m[INFO] Сервер запущен\033[0m")
         
         while True:
             conn, addr = sock.accept()
-            print(f"\n[INFO] Установлено новое соединение: {addr}")
+            print(f"\n\033[94m[INFO] Установлено новое соединение: {addr}\033[0m")
             
-            conn.send((bytes(str(self.n), encoding = "utf8")))
+            conn.send((bytes(str(self.n), encoding="utf8")))
 
-            print(f"[INFO] Пользователю {addr} отправлено n")
+            print(f"\033[94m[INFO] Пользователю {addr} отправлено n\033[0m")
             
             data = conn.recv(self._buffer_size).decode("utf8")
             
@@ -50,57 +49,54 @@ class Server:
                 v = int(conn.recv(self._buffer_size).decode("utf8"))
                 
                 if name not in self._registered_users.keys():
-                    print(f"[INFO] Пользователь {addr} зарегистрировался с никнеймом '{name}' и ключом {v}")
+                    print(f"\033[92m[INFO] Пользователь {addr} зарегистрировался с никнеймом '{name}' и ключом {v}\033[0m")
                     self._registered_users[name] = v
                     with open("registered_users.json", "w") as f:
                         json.dump(self._registered_users, f)
-                    conn.send((bytes("success", encoding = "utf8")))
+                    conn.send((bytes("success", encoding="utf8")))
                 else:
-                    print(f"[ERROR] Никнейм '{name}' уже зарегистрирован")
-                    conn.send((bytes("already registered", encoding = "utf8")))
+                    print(f"\033[91m[ERROR] Никнейм '{name}' уже зарегистрирован\033[0m")
+                    conn.send((bytes("already registered", encoding="utf8")))
 
             
             if data == "{auth}":
                 name = conn.recv(self._buffer_size).decode("utf8")
                 v = self._registered_users[name]
                 
-                print(f"[INFO] Пользователь {addr} пытается авторизироваться под никнеймом '{name}'")
+                print(f"\033[94m[INFO] Пользователь {addr} пытается авторизироваться под никнеймом '{name}'\033[0m")
 
-                print(f"[INFO] Аутентификация:")
+                print(f"\033[94m[INFO] Аутентификация:\033[0m")
                 
                 t = 20
-                for i, _ in enumerate(range(t), 1):
+                for i in range(t):
                     x = int(conn.recv(self._buffer_size).decode("utf8"))
 
                     e = random.randint(0, 1)
-                    conn.send((bytes(str(e), encoding = "utf8")))
-
+                    conn.send((bytes(str(e), encoding="utf8")))
 
                     y = int(conn.recv(self._buffer_size).decode("utf8"))
 
                     y2 = exponentiation_modulo(y, 2, self.n)
                     xv = x * exponentiation_modulo(v, e, self.n) % self.n
-                    print(f"{i}.\n\t{x = }\n\t{e = }\n\t{y = }\n\t--\n\ty^2 % n = {y2}\n\tx * v^e % n = {xv}")
+                    print(f"{i + 1}.\n\t{x=}\n\t{e=}\n\t{y=}\n\t--\n\ty^2 % n = {y2}\n\tx * v^e % n = {xv}")
                     
                     if y2 == xv:
-                        print(f"(success) y^2 == x * v^e % n\n")
-                        if i == t:
-                            print(f"[INFO] Аутентификация '{name}' пройдена успешно")
-                            conn.send((bytes("success", encoding = "utf8")))
+                        print(f"\t(success) y^2 == x * v^e % n\n")
+                        if i + 1 == t:
+                            print(f"\033[92m[INFO] Аутентификация '{name}' пройдена успешно\033[0m")
+                            conn.send((bytes("success", encoding="utf8")))
                         else:
-                            conn.send((bytes("check", encoding = "utf8")))
+                            conn.send((bytes("check", encoding="utf8")))
                     else:
-                        print(f"(fail) y^2 != x * v^e % n\n")
-                        conn.send((bytes("fail", encoding = "utf8")))
-                        print(f"[INFO] Аутентификация '{name}' не пройдена. Пользователь {addr} получает бан по ip")
+                        print(f"\t(fail) y^2 != x * v^e % n\n")
+                        conn.send((bytes("fail", encoding="utf8")))
+                        print(f"\033[91m[INFO] Аутентификация '{name}' не пройдена. Пользователь {addr} получает бан по ip\033[0m")
                         break
 
             conn.close()
-            print(f"[INFO] Соединение разорвано")
-        
-if __name__ == "__main__":
+            print(f"\033[94m[INFO] Соединение разорвано\033[0m")
 
+if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     server = Server()
     server.run()
-    
